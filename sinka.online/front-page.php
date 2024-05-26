d7cf1b84a28a4e870d66f56964b2870c4322a99d
Noriyuki Mizuno <inc.sinka@protonmail.com>
2024-05-27 00:52:02 +0900

    p-mv-first-commit

--- sinka.online/front-page.php
+++ sinka.online/front-page.php
@@ -1,8 +1,17 @@
 <?php get_header(); ?>
 
 <div class="p-mv">
-  <div class="p-mv__inner">
-    
+  <div class="p-mv__content">
+    <figure class="p-mv__content-img">
+      <picture>
+        <source media="(max-width: 768px)" srcset="<?php echo get_template_directory_uri(); ?>/assets/images/mv-img-mobile.png">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/mv-logo.jpeg" alt="sinkaのロゴです" width="1920" height="1080">
+      </picture>
+    </figure>
+    <div class="p-mv__content-text">
+      <h2 class="p-mv__content-title">ビジネスをSiNkAさせます</h2>
+      <p class="p-mv__content-lead">sinkaはWeb制作を通してビジネスの<br>進化につながるお手伝いをします</p>
+    </div>
   </div>
 </div>
 
