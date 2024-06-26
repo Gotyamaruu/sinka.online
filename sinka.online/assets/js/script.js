9b49149cd095638889833b5935dd8c2bdb7e2874
Noriyuki Mizuno <inc.sinka@protonmail.com>
2024-06-27 01:59:04 +0900

    p-service-card-section-animetions-tool-vertions_add

--- sinka.online/assets/js/script.js
+++ sinka.online/assets/js/script.js
@@ -1,100 +1,162 @@
-// ページトップボタン
-var topBtn = $(".js-pagetop");
-topBtn.hide();
+document.addEventListener("DOMContentLoaded", function () {
+  gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
-// ページトップボタンの表示設定
-$(window).scroll(function () {
-  if ($(this).scrollTop() > 70) {
-    // 指定px以上のスクロールでボタンを表示
-    topBtn.fadeIn();
-  } else {
-    // 画面が指定pxより上ならボタンを非表示
-    topBtn.fadeOut();
-  }
-});
+  // ページトップボタン
+  const topBtn = document.querySelector(".js-pagetop");
+  topBtn.style.display = "none";
 
-// ページトップボタンをクリックしたらスクロールして上に戻る
-topBtn.click(function () {
-  $("body,html").animate(
-    {
-      scrollTop: 0,
+  // ページトップボタンの表示設定
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 70) {
+      topBtn.style.display = "block";
+    } else {
+      topBtn.style.display = "none";
+    }
+  });
+
+  // ページトップボタンをクリックしたらスクロールして上に戻る
+  topBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+
+  // スムーススクロール (絶対パスのリンク先が現在のページであった場合でも作動。ヘッダーの高さ考慮。)
+  document.querySelectorAll('a[href*="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute("href").substring(1);
+      const targetElement = document.getElementById(targetId);
+      if (!targetElement) return;
+      const headerHeight = document.querySelector("header").offsetHeight;
+      const targetPosition =
+        targetElement.getBoundingClientRect().top +
+        window.pageYOffset -
+        headerHeight;
+      window.scrollTo({
+        top: targetPosition,
+        behavior: "smooth",
+      });
+    });
+  });
+
+  particlesJS("particles-js", {
+    particles: {
+      number: { value: 140, density: { enable: true, value_area: 780 } },
+      color: { value: "#ffffff" },
+      shape: {
+        type: "circle",
+        stroke: { width: 0, color: "#000000" },
+        polygon: { nb_sides: 5 },
+        image: { src: "img/github.svg", width: 100, height: 100 },
+      },
+      opacity: {
+        value: 0.5,
+        random: false,
+        anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
+      },
+      size: {
+        value: 3,
+        random: true,
+        anim: { enable: false, speed: 40, size_min: 0.1, sync: false },
+      },
+      line_linked: {
+        enable: true,
+        distance: 110.48066982851817,
+        color: "#ffffff",
+        opacity: 1,
+        width: 1.5,
+      },
+      move: {
+        enable: true,
+        speed: 6,
+        direction: "none",
+        random: false,
+        straight: false,
+        out_mode: "out",
+        bounce: false,
+        attract: { enable: false, rotateX: 600, rotateY: 1200 },
+      },
     },
-    300,
-    "swing"
+    interactivity: {
+      detect_on: "canvas",
+      events: {
+        onhover: { enable: true, mode: "repulse" },
+        onclick: { enable: true, mode: "push" },
+        resize: true,
+      },
+      modes: {
+        grab: { distance: 400, line_linked: { opacity: 1 } },
+        bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
+        repulse: { distance: 200, duration: 0.4 },
+        push: { particles_nb: 4 },
+        remove: { particles_nb: 2 },
+      },
+    },
+    retina_detect: true,
+  });
+
+  const contentAnimateElement = document.querySelector(
+    ".p-mv__content-animate"
   );
-  return false;
-});
-
-// スムーススクロール (絶対パスのリンク先が現在のページであった場合でも作動。ヘッダーの高さ考慮。)
-$(document).on("click", 'a[href*="#"]', function () {
-  let time = 400;
-  let header = $("header").innerHeight();
-  let target = $(this.hash);
-  if (!target.length) return;
-  let targetY = target.offset().top - header;
-  $("html,body").animate({ scrollTop: targetY }, time, "swing");
-  return false;
-});
-
-particlesJS("particles-js", {
-  particles: {
-    number: { value: 140, density: { enable: true, value_area: 780 } },
-    color: { value: "#ffffff" },
-    shape: {
-      type: "circle",
-      stroke: { width: 0, color: "#000000" },
-      polygon: { nb_sides: 5 },
-      image: { src: "img/github.svg", width: 100, height: 100 },
-    },
-    opacity: {
-      value: 0.5,
-      random: false,
-      anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
-    },
-    size: {
-      value: 3,
-      random: true,
-      anim: { enable: false, speed: 40, size_min: 0.1, sync: false },
-    },
-    line_linked: {
-      enable: true,
-      distance: 110.48066982851817,
-      color: "#ffffff",
-      opacity: 1,
-      width: 1.5,
-    },
-    move: {
-      enable: true,
-      speed: 6,
-      direction: "none",
-      random: false,
-      straight: false,
-      out_mode: "out",
-      bounce: false,
-      attract: { enable: false, rotateX: 600, rotateY: 1200 },
-    },
-  },
-  interactivity: {
-    detect_on: "canvas",
-    events: {
-      onhover: { enable: true, mode: "repulse" },
-      onclick: { enable: true, mode: "push" },
-      resize: true,
-    },
-    modes: {
-      grab: { distance: 400, line_linked: { opacity: 1 } },
-      bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
-      repulse: { distance: 200, duration: 0.4 },
-      push: { particles_nb: 4 },
-      remove: { particles_nb: 2 },
-    },
-  },
-  retina_detect: true,
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-  const contentAnimateElement = document.querySelector('.p-mv__content-animate');
   if (contentAnimateElement) {
-    contentAnimateElement.classList.add('load-animation');
+    contentAnimateElement.classList.add("load-animation");
   }
+
+  document.querySelectorAll(".js-visible").forEach((section) => {
+    const sectionTitle = section.querySelector(".c-section-title");
+
+    gsap.fromTo(
+      section,
+      { autoAlpha: 0, y: 50 },
+      {
+        autoAlpha: 1,
+        y: 0,
+        scrollTrigger: {
+          trigger: section,
+          start: "top 90%",
+          end: "bottom top",
+          toggleActions: "play none none reverse",
+          onEnter: () => sectionTitle.classList.add("load-animation"),
+          onLeaveBack: () => sectionTitle.classList.remove("load-animation"),
+        },
+      }
+    );
+  });
+
+  const hamburger = document.querySelector(".js-hamburger");
+  const drawer = document.querySelector(".js-drawer");
+
+  hamburger.addEventListener("click", function () {
+    hamburger.classList.toggle("open");
+    if (hamburger.classList.contains("open")) {
+      gsap.to(drawer, { display: "flex", duration: 0.3, ease: "power2.inOut" });
+      drawer.classList.add("open");
+    } else {
+      gsap.to(drawer, { display: "none", duration: 0.3, ease: "power2.inOut" });
+      drawer.classList.remove("open");
+    }
+  });
+
+  window.addEventListener("load", resizeHandler);
+  window.addEventListener("resize", resizeHandler);
+
+  function resizeHandler() {
+    const width = window.innerWidth;
+    if (width >= 768) {
+      gsap.to(drawer, { display: "flex", duration: 0 });
+      dr
+      
+      awer.classList.remove("open");
+    } else {
+      gsap.to(drawer, { display: "none", duration: 0 });
+    }
+  }
+
+  ScrollTrigger.create({
+    start: "top -50", 
+    toggleClass: { targets: ".p-header", className: "js-scrolled" },
+  });
 });
--- sinka.online/front-page.php
+++ sinka.online/front-page.php
@@ -1,23 +1,25 @@
 
   <?php get_header(); ?>
 
+  <!-- ページトップボタン -->
+  <a href="#" class="js-pagetop">Top</a>
   <section class="p-mv">
     <div class="p-mv__content">
-      <div class="p-mv__content-text">
-        <div class="p-mv__content-animate">
+      <div class="p-mv__content-animate">
+        <div class="p-mv__content-text">
           <h1 class="p-mv__content-title">WebでビジネスをSiNkA</h1>
           <p class="p-mv__content-lead">Advance your business on the web</p>
         </div>
       </div>
     </div>
   </section>
-  <section class="p-service">
+  <section class="p-service js-visible">
     <div class="l-inner">
-      <h2 class="c-section-title">Service</h2>
+      <h2 class="p-service__title c-section-title">Service</h2>
       <div class="p-service__content">
         <div class="p-service__content-item">
           <figure class="p-service__content-img">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/service-3.jpeg" alt="HP/LP制作の画像です" width="250" height="250">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/service-3.jpeg" alt="HP/LP制作の画像です" width="250" height="150">
           </figure>
           <h3 class="p-service__content-title">HP/LP制作</h3>
           <p class="p-service__content-lead">お客様のビジネスに合わせたWebサイトを提供します</p>
@@ -27,17 +29,20 @@
             <img src="<?php echo get_template_directory_uri(); ?>/assets/images/service-2.jpeg" alt="サービス2の画像です" width="250" height="250">
           </figure>
           <h3 class="p-service__content-title">Web Development</h3>
-          <p class="p-service__content-lead">お客様のビジネスに合わせた開発を提供します</p>
+          <p class="p-service__content-lead">お客様のビジネスに合わせた<br>開発を提供します</p>
         </div>
         <div class="p-service__content-item">
           <figure class="p-service__content-img">
             <img src="<?php echo get_template_directory_uri(); ?>/assets/images/service-1.jpeg" alt="サービス2の画像です" width="250" height="250">
           </figure>
           <h3 class="p-service__content-title">Web Development</h3>
-          <p class="p-service__content-lead">お客様のビジネスに合わせた開発を提供します</p>
+          <p class="p-service__content-lead">お客様のビジネスに合わせた<br>開発を提供します</p>
         </div>
       </div>
     </div>
+  </section>
+  <section class="p-works js-visible">
+    
   </section>
 
   <?php get_footer(); ?>
